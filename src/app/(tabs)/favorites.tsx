79b31a2ad5d0c9c39a434a0efd6ab7871ce25957
Qujuan <qujuan.miller@snhu.edu>
2024-04-24 08:51:53 -0400

    init and clean

--- src/app/(tabs)/favorites.tsx
+++ src/app/(tabs)/favorites.tsx
@@ -1,12 +1,12 @@
+import EditScreenInfo from '@/src/components/EditScreenInfo';
+import { View, Text } from '@/src/components/Themed';
 import { StyleSheet } from 'react-native';
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
 
-export default function TabTwoScreen() {
+export default function FavoritesScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
+      <Text style={styles.title}>Favorites</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/two.tsx" />
     </View>
